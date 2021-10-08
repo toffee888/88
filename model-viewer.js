5b9ecc38f4d7b20077e5d041e535c4e2fc6b8b57
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-09 02:05:47 +0700

    Update model-viewer.js

--- model-viewer.js
+++ model-viewer.js
@@ -359,7 +359,7 @@ AFRAME.registerComponent('model-viewer', {
     // Reset position and scales.
     modelEl.object3D.position.set(0, 0, -5);
     modelEl.object3D.scale.set(0.7, 0.7, 0.7);
-    this.cameraRigEl.object3D.position.z = 3.0;
+    this.cameraRigEl.object3D.position.z = 3.1;
 
     // Calculate model size.
     modelEl.object3D.updateMatrixWorld();
