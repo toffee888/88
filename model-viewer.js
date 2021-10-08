11303567c6742b1ae0d96346012c34d5f62c7e0a
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-09 01:48:46 +0700

    Update model-viewer.js

--- model-viewer.js
+++ model-viewer.js
@@ -50,8 +50,6 @@ AFRAME.registerComponent('model-viewer', {
     this.laserHitPanelEl.addEventListener('mousedown', this.onMouseDownLaserHitPanel);
     this.laserHitPanelEl.addEventListener('mouseup', this.onMouseUpLaserHitPanel);
 
-    this.leftHandEl.addEventListener('thumbstickmoved', this.onThumbstickMoved);
-    this.rightHandEl.addEventListener('thumbstickmoved', this.onThumbstickMoved);
 
     // Mouse 2D controls.
     document.addEventListener('mouseup', this.onMouseUp);
@@ -63,59 +61,16 @@ AFRAME.registerComponent('model-viewer', {
     document.addEventListener('touchend', this.onTouchEnd);
     document.addEventListener('touchmove', this.onTouchMove);
 
-    this.el.sceneEl.addEventListener('enter-vr', this.onEnterVR);
-    this.el.sceneEl.addEventListener('exit-vr', this.onExitVR);
 
     this.modelEl.addEventListener('model-loaded', this.onModelLoaded);
   },
 
   initUploadInput: function () {
-    var uploadContainerEl = this.uploadContainerEl = document.createElement('div');
-    var inputEl = this.inputEl = document.createElement('input');
-    var submitButtonEl = this.submitButtonEl = document.createElement('button');
-    var style = document.createElement('style');
-    var css =
-      '.a-upload-model  {box-sizing: border-box; display: inline-block; height: 34px; padding: 0; width: 70%;' +
-      'bottom: 20px; left: 15%; right: 15%; position: absolute; color: white;' +
-      'font-size: 12px; line-height: 12px; border: none;' +
-      'border-radius: 5px}' +
-      '.a-upload-model.hidden {display: none}' +
-      '.a-upload-model-button {cursor: pointer; padding: 0px 2px 0 2px; font-weight: bold; color: #666; border: 3px solid #666; box-sizing: border-box; vertical-align: middle; width: 25%; max-width: 110px; border-radius: 10px; height: 34px; background-color: white; margin: 0;}' +
-      '.a-upload-model-button:hover {border-color: #ef2d5e; color: #ef2d5e}' +
-      '.a-upload-model-input {color: #666; vertical-align: middle; padding: 0px 10px 0 10px; text-transform: uppercase; border: 0; width: 75%; height: 100%; border-radius: 10px; margin-right: 10px}' +
-      '@media only screen and (max-width: 800px) {' +
-      '.a-upload-model {margin: auto;}' +
-      '.a-upload-model-input {width: 70%;}}' +
-      '@media only screen and (max-width: 700px) {' +
-      '.a-upload-model {display: none}}';
-    var inputDefaultValue = this.inputDefaultValue = 'Copy URL to glTF or glb model';
 
     if (AFRAME.utils.device.checkARSupport()) {
       css += '@media only screen and (max-width: 800px) {' +
       '.a-upload-model-input {width: 60%;}}';
     }
-
-    uploadContainerEl.classList.add('a-upload-model');
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-    document.getElementsByTagName('head')[0].appendChild(style);
-
-    submitButtonEl.classList.add('a-upload-model-button');
-    submitButtonEl.innerHTML = 'OPEN MODEL';
-    submitButtonEl.addEventListener('click', this.submitURLButtonClicked);
-
-    inputEl.classList.add('a-upload-model-input');
-    inputEl.onfocus = function () {
-      if (this.value !== inputDefaultValue) { return; }
-      this.value = '';
-    };
-    inputEl.onblur = function () {
-      if (this.value) { return; }
-      this.value = inputDefaultValue;
-    };
 
     this.el.sceneEl.addEventListener('infomessageopened', function () {
       uploadContainerEl.classList.add('hidden');
