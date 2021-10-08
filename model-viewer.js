780090a320af58f6527da1741192cdf3a4290bb4
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-09 01:59:20 +0700

    Update model-viewer.js

--- model-viewer.js
+++ model-viewer.js
@@ -107,18 +107,6 @@ AFRAME.registerComponent('model-viewer', {
     this.el.appendChild(cameraRigEl);
   },
 
-  initBackground: function () {
-    var backgroundEl = this.backgroundEl = document.querySelector('a-entity');
-    backgroundEl.setAttribute('geometry', {primitive: 'sphere', radius: 65});
-    backgroundEl.setAttribute('material', {
-      shader: 'background-gradient',
-      colorTop: '#37383c',
-      colorBottom: '#757575',
-      side: 'back'
-    });
-    backgroundEl.setAttribute('hide-on-enter-ar', '');
-  },
-
   initEntities: function () {
     // Container for our entities to keep the scene clean and tidy.
     var containerEl = this.containerEl = document.createElement('a-entity');
@@ -216,14 +204,6 @@ AFRAME.registerComponent('model-viewer', {
     this.el.appendChild(reticleEl);
   },
 
-  onThumbstickMoved: function (evt) {
-    var modelPivotEl = this.modelPivotEl;
-    var modelScale = this.modelScale || modelPivotEl.object3D.scale.x;
-    modelScale -= evt.detail.y / 20;
-    modelScale = Math.min(Math.max(0.8, modelScale), 2.0);
-    modelPivotEl.object3D.scale.set(modelScale, modelScale, modelScale);
-    this.modelScale = modelScale;
-  },
 
   onMouseWheel: function (evt) {
     var modelPivotEl = this.modelPivotEl;
