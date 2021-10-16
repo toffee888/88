a953a031d2391ab78b61936abf0f6268010c8b87
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-16 21:50:17 +0700

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -10,6 +10,9 @@ AFRAME.registerComponent('gesture-handler', {
 
     init: function() {
         this.handleScale = this.handleScale.bind(this);
+        this.handleRotation = this.handleRotation.bind(this);
+
+        this.isVisible = false;
         this.initialScale = this.el.object3D.scale.clone();
         this.scaleFactor = 1;
         this.isVisible = true;
@@ -18,16 +21,27 @@ AFRAME.registerComponent('gesture-handler', {
 
     update: function() {
         if (this.data.enabled) {
+            this.el.sceneEl.addEventListener('onefingermove', this.handleRotation);
             this.el.sceneEl.addEventListener('twofingermove', this.handleScale);
         } else {
+            this.el.sceneEl.removeEventListener('onefingermove', this.handleRotation);
             this.el.sceneEl.removeEventListener('twofingermove', this.handleScale);
         }
     },
 
     remove: function() {
+        this.el.sceneEl.removeEventListener('onefingermove', this.handleRotation);
         this.el.sceneEl.removeEventListener('twofingermove', this.handleScale);
     },
 
+    handleRotation: function(event) {
+        if (this.isVisible) {
+            this.el.object3D.rotation.y +=
+                event.detail.positionChange.x * this.data.rotationFactor;
+            this.el.object3D.rotation.x +=
+                event.detail.positionChange.y * this.data.rotationFactor;
+        }
+    },
 
     handleScale: function(event) {
         if (this.isVisible) {
