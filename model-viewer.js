f5f2b5b17b500a803af7d1d1a5cc885f73c86f49
toffee888 <86227265+toffee888@users.noreply.github.com>
2021-10-16 22:31:38 +0700

    Update model-viewer.js

--- model-viewer.js
+++ model-viewer.js
@@ -159,17 +159,17 @@ AFRAME.registerComponent('model-viewer', {
 
     modelPivotEl.appendChild(modelEl);
 
-    shadowEl.setAttribute('rotation', '90 -30 0');
+    shadowEl.setAttribute('rotation', '-90 -30 0');
     shadowEl.setAttribute('geometry', 'primitive: plane; width: 1.0; height: 1.0');
     shadowEl.setAttribute('material', 'src: #shadow; transparent: true; opacity: 1');
     shadowEl.setAttribute('hide-on-enter-ar', '');
 
     modelPivotEl.appendChild(shadowEl);
 
-    arShadowEl.setAttribute('rotation', '90 0 0');
+    arShadowEl.setAttribute('rotation', '-90 0 0');
     arShadowEl.setAttribute('geometry', 'primitive: plane; width: 30.0; height: 30.0');
     arShadowEl.setAttribute('shadow', 'recieve: true');
-    arShadowEl.setAttribute('ar-shadows', 'opacity: 0');
+    arShadowEl.setAttribute('ar-shadows', 'opacity: 1');
     arShadowEl.setAttribute('visible', 'false');
 
     modelPivotEl.appendChild(arShadowEl);
